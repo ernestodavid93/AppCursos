3059408263a88266abb7308650917e949bd8ec0b
Ernesto David <locodecumbia@gmail.com>
2021-12-27 11:21:19 -0600

    Seeders y Factories completo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Course;
+use Illuminate\Contracts\Cache\Store;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
 
@@ -17,12 +18,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //User::factory(10)->create();
-        Storage::makeDirectory('cursos');
+        Storage::deleteDirectory('cursos'); //Eliminamos primero para que no se acumulen las imagenes
+        Storage::makeDirectory('cursos'); //Crea la carpeta en el storage
 
         $this->call(UserSeeder::class);
         $this->call(LevelSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(PriceSeeder::class);
+        $this->call(PlatformSeeder::class);
         $this->call(CourseSedeer::class);
     }
 }
